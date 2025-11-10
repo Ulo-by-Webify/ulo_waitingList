e10ea517f42ede2fb50bea1fa16d9323578c9a17
Tnath1 <aromejonathanu@gmail.com>
2025-11-10 18:01:01 +0100

    setting up

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,74 @@
+import React, { useRef } from "react";
+import { HeroContent } from "@/types";
+import BlurText from "@/components/BlurText";
+import { useMobileVideoPlayback } from "../hooks/useMobileVideoPlayback";
+
+interface HeroSectionProps {
+  heroContent: HeroContent;
+}
+
+const handleAnimationComplete = () => {
+  console.log("Animation completed!");
+};
+
+const handleScrollToVideo = () => {
+  const videoSection = document.getElementById("video-section");
+  if (videoSection) {
+    videoSection.scrollIntoView({ behavior: "smooth" });
+  }
+};
+
+const HeroSection: React.FC<HeroSectionProps> = ({ heroContent }) => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  useMobileVideoPlayback(videoRef);
+
+  return (
+    <section className="relative h-screen flex items-center justify-center px-4 sm:px-6 lg:px-8 overflow-hidden">
+      {/* Background Video */}
+      <video
+        // ref={videoRef}
+        className="absolute inset-0 w-full h-full object-cover z-0"
+        src="/videos/hero-page.mp4"
+        autoPlay
+        muted
+        loop
+        playsInline
+        preload="auto"
+      >
+        {/* <source  type="video/mp4" /> */}
+        Your browser does not support the video tag.
+      </video>
+
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-black/20 z-10"></div>
+
+      {/* Content */}
+      <div className="relative z-20 max-w-sm md:max-w-5xl p-5 mx-auto mt-56 text-center">
+        <BlurText
+          suffix={["Africa", "Lagos", "Durban", "Accra", "Nairobi", "Kigali"]}
+          cycleInterval={3000}
+          text="Don’t just visit"
+          bottomText="Connect with it."
+          animateBy="words"
+          direction="top"
+          onAnimationComplete={handleAnimationComplete}
+          className="text-[2.2rem] sm:text-6xl md:text-8xl text-white mb-3 tracking-tight font-medium leading-none md:leading-tight"
+        />
+        <p className="text-lg text-white mb-40 md:px-20 leading-relaxed animate-fade-in max-w-3xl mx-auto tracking-wide font-light">
+          {heroContent.description}
+        </p>
+
+        <div className="max-w-14 mx-auto animate-bounce duration-1000">
+          <button
+            onClick={handleScrollToVideo}
+            aria-label="Scroll to video section"
+          >
+            <img src="/icons/ARROW.svg" alt="" className="scale-125" />
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
---
+++ src/pages/Index.tsx
@@ -0,0 +1,129 @@
+import React, { useRef } from 'react'
+import Header from '@/components/Header'
+import HeroSection from '@/components/HeroSection'
+import VideoShowcaseSection from '@/components/VideoShowcaseSection'
+import VideoContentSection from '@/components/VideoContentSection'
+import FeaturesSection from '@/components/FeaturesSection'
+import BusinessShowcaseSection from '@/components/BusinessShowcaseSection'
+import HowToJoinSection, {
+  HowToJoinSectionRef
+} from '@/components/HowToJoinSection'
+import CTASection from '@/components/CTASection'
+import FAQSection from '@/components/FAQSection'
+import Footer from '@/components/Footer'
+import ScrollDotsSection from '@/components/ScrollDotsSection'
+import AncillaryServicesShowcase from '@/components/AncillaryServicesShowcase'
+import { heroContent, businessSlides, flippedSlides } from '@/data'
+import MusicToggleButton from '@/components/MusicToggleButton'
+import { useIsMobile } from '@/hooks/use-mobile'
+
+const Index = () => {
+  const isMobile = useIsMobile()
+  console.log(isMobile)
+  const howToJoinSectionRef = useRef<HowToJoinSectionRef>(null)
+  return (
+    <div className="min-h-screen bg-white">
+      <Header howToJoinSectionRef={howToJoinSectionRef} />
+
+      <div className="">
+        <HeroSection heroContent={heroContent} />
+
+        <VideoShowcaseSection heroContent={heroContent} />
+
+        <ScrollDotsSection />
+
+        <VideoContentSection
+          title1="Hands‑Free Management"
+          subtitle1="From bookings to check-outs, Ulô handles every detail. This means you earn effortlessly, while your guests enjoy a flawless stay."
+          video1="/videos/Hands‑Free-Management/1.webm"
+          title2="Focus on what matters most"
+          subtitle2="With Ulô Associates handling the chats, check-ins, and care, hosting feels easy — and guests feel at home."
+          video2="/videos/Hands‑Free-Management/2.webm"
+          listItems={[
+            'Automated booking management',
+            '24/7 guest communication & support',
+            'Seamless check-in, check-out & cleaning'
+          ]}
+          sectionId="handsfree-section"
+        />
+
+        <VideoContentSection
+          title1="Xperience Gallery"
+          subtitle1="From vibrant cities to local secrets, Ulô uncovers it all, helping you explore, connect, and make travel unforgettable."
+          video1="https://res.cloudinary.com/dfcsaxtru/video/upload/q_70/v1756200081/INTRO_XPE-_1_yoovjb.mp4"
+          title2="See the soul of every stay"
+          subtitle2="Ulô’s Xperience Gallery reveals the unique character of every home and journey, building the trust and connection that inspires your next adventure."
+          video2="https://res.cloudinary.com/dfcsaxtru/video/upload/q_70/v1756200146/XPE._DEMO_FULL_li5hau.mp4"
+          listItems={[
+            'Explore authentic reels of vibrant cities and local secrets.',
+            'Create & share your unique city story with friends and family.',
+            'Feel the home’s aura through video reels before you book.'
+          ]}
+          sectionId="xperience-section"
+        />
+
+        <VideoContentSection
+          title1="Borderless Payment"
+          subtitle1="Pay with confidence. Ulô Cowries makes every transaction, from booking to last-minute extras, simple, seamless, and secure."
+          video1="https://res.cloudinary.com/dfcsaxtru/video/upload/q_70/v1756200048/PAYMENT_pkdt2y.mp4"
+          title2="Seamless Journeys, Simple Payments"
+          subtitle2="Ulô Cowries makes paying simple and worry-free. You get to pay your way, so your focus stays on the adventure, not the transaction."
+          video2="https://res.cloudinary.com/dfcsaxtru/video/upload/q_70/v1756200053/HOME_PAYMENT_qnvakc.mp4"
+          listItems={[
+            'Convert with USD valuation — 1 ACW = 1 USD',
+            'Share Cowries easily with family & friends',
+            'Withdraw in any African currency with ease'
+          ]}
+          sectionId="borderless-section"
+        />
+
+        <VideoContentSection
+          title1="Ulô Associates"
+          subtitle1="Your personal city companion. Managing your stay, guiding your steps, and making every moment smoother."
+          video1="https://res.cloudinary.com/dfcsaxtru/video/upload/q_70/v1756200085/INTRO_TOUR_cycmao.mp4"
+          title2="Feel the city, not the stress"
+          subtitle2="With Ulô Associates, guests are welcomed like family, guided through culture and hidden gems, and supported every step for a truly effortless stay."
+          video2="https://res.cloudinary.com/dfcsaxtru/video/upload/q_70/v1756200069/GUEST_TOUR_FULL_oc7fdk.mp4"
+          listItems={[
+            ' Welcome you at check-in and ensure comfort',
+            ' Connect you with culture, tours, and insider experiences',
+            ' ⁠Handle safety, logistics, and local support'
+          ]}
+          sectionId="associates-section"
+        />
+        <FeaturesSection />
+
+        {/* <BusinessShowcaseSection businessSlides={businessSlides} /> */}
+
+        <AncillaryServicesShowcase
+          slides={flippedSlides}
+          sectionTitle="Go Beyond the Stay"
+        />
+
+        <VideoContentSection
+          title1="Guest Handbook"
+          subtitle1="Your passport to belonging. This city companion has everything you need to explore African cities with confidence, from key phrases to etiquette and safe spots."
+          video1="https://res.cloudinary.com/dfcsaxtru/video/upload/v1756208494/INTRO_HANDBOOK_1_et6nsp.mp4"
+          listItems={[
+            'Navigate easily',
+            'Connect authentically',
+            'Access help when needed'
+          ]}
+          sectionId="guestbook-section"
+          isLooped={false}
+        />
+        <CTASection />
+
+        <HowToJoinSection ref={howToJoinSectionRef} />
+
+        <FAQSection />
+
+        <Footer />
+      </div>
+      {/* 
+      <MusicToggleButton containerClassName="fixed left-8 bottom-8 z-50 hover:opacity-100 opacity-70" /> */}
+    </div>
+  )
+}
+
+export default Index
